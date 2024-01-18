7a80a02dc4fa79eac0ce6dc375dc97f1b7044762
Abhishek <abhisheksunny463@gmail.com>
2024-01-19 00:53:04 +0530

    Blog Website

--- Blog Website/backend/controllers/auth.js
+++ Blog Website/backend/controllers/auth.js
@@ -5,18 +5,38 @@ const handleSignUp = async (req,res) => {
     try {
         const {name, email, password} = req.body
         
-        await Auth.create({name, email, password})
-        res.status(201).
+        const userExists = await Auth.findOne({email})
+
+        if(userExists){
+            res.status(404)
+            throw new Error("User Already Exists")
+        }
+
+        const user = await Auth.create({name, email, password})
+        
+        if (user) {
+            res.status(201).json({
+                _id: user._id, name:user.name, email:user.email
+            })
+        } else {
+            res.status(400)
+            throw new Error("Something wents wrong.")
+        }
 
     } catch (error) {
-        
+        res.status(400)
+        throw new Error(error)
     }
 
 }
 
 
 const handleSignIn = async (req,res) => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 module.exports = {handleSignIn, handleSignUp}
