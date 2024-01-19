4365d51f918c7453d67163b1d049b2b0fe6a0e69
Abhishek <abhisheksunny463@gmail.com>
2024-01-19 22:28:27 +0530

    Blog Website

--- Blog Website/frontend/src/Components/Pages/Signin.jsx
+++ Blog Website/frontend/src/Components/Pages/Signin.jsx
@@ -1,19 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Signin = () => {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
+
+    const showErrorAlert = (message) => {
+        setErrorMessage(message);
+        setTimeout(() => {
+            setErrorMessage('');
+        }, 5000);
+    };
+
+    const clearForm = () => {
+        setFormData({
+            name: '',
+            email: '',
+            password: ''
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json"
+                },
+            }
+
+            const response = await axios.post('http://localhost:8001/api/auth/signup', {
+                name: formData.name, email: formData.email, password: formData.password
+            }, config)
+
+            console.log(response.data)
+
+            clearForm()
+
+        } catch (error) {
+            showErrorAlert('Invalid email or password');
+        }
+
+    }
+
     return (
         <div className='signin vh-100 d-flex align-items-center justify-content-center'>
-            <form className='w-25 shadow-lg p-3'>
+            <form className='w-25 shadow-lg p-3' method='POST' onSubmit={handleSubmit}>
+            {errorMessage && <div class="popup-error m-3 position-fixed top-0 end-0" role="alert">{errorMessage}</div>}
                 <h4 className='text-center text-secondary'>Sign In Here</h4>
                 <hr />
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" required />
+                    <input type="email" className="form-control" id="exampleInputEmail1"
+                        name='email'
+                        value={formData.email}
+                        onChange={handleChange}
+                        required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" required />
+                    <input type="password" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp"
+                        name='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                        required />
                     <div id="emailHelp" className="form-text">We'll never share your password with anyone else.</div>
                 </div>
                 <button type="submit" className="btn w-100 btn-primary">Sign In</button>
