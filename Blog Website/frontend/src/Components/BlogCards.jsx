7ef1bd7da596b8c4059b773e6de60bb852e3d85e
Abhishek <abhisheksunny463@gmail.com>
2024-01-23 03:21:06 +0530

    Blog Website

--- Blog Website/frontend/src/Components/BlogCards.jsx
+++ Blog Website/frontend/src/Components/BlogCards.jsx
@@ -1,58 +1,65 @@
 import React, { useEffect, useState } from 'react'
 import img1 from '../Assests/pexels-plann-4549414.jpg'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const Card = () => {
-
-  return(
+const Card = ({blog}) => {
+  const { _id, title, category, content, createdAt, coverImage } = blog
+  return (
     <>
       <div className="blog-card shine">
         <div className="image-section">
-          <img src={img1} alt=""/>
+          <img src={coverImage} alt={blog._id} />
         </div>
+
         <div className="description-section">
-          <h6>There's going to be a musical about meghan</h6>
+          <h6>{title}</h6>
           <div className="date">
             <p><i class="fa-regular fa-calendar mx-1"></i>20th Nov, 2023</p>
             <p className='mx-5'><i class="fa-regular fa-message mx-1"></i>05</p>
           </div>
           <p className="caption">
-          Creepeth green light appear let rule only you're divide and lights moving and isn't given creeping deep.
+            {content.slice(0,100)}...
           </p>
 
           <div className="links">
             <Link>READ MORE<i class="fa-solid fa-arrow-right-long mx-2"></i></Link>
-            <p><i class="fa-regular fa-folder-closed mx-1"></i>Travel</p>
+            <p><i class="fa-regular fa-folder-closed mx-1"></i>{category}</p>
           </div>
         </div>
       </div>
     </>
   )
-  
+
 }
 
 const BlogCards = () => {
 
   const [blogs, setBlogs] = useState([])
 
-  useEffect(()=> {
-    
-  }, [])
+  const getBlogs = async () => {
+    try {
+      const response = await axios.get('http://localhost:8001/api/blog');
+      const data = await response?.data;
+      setBlogs(data?.data);
+      console.log("API Response:", data.data);
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+    }
+  };
 
-  const getBlogs = async()=> {
-    const response = axios.get
-  }
+  useEffect(() => {
+    getBlogs()
+  }, [])
 
   return (
     <div className='blog-cards'>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+      {
+          blogs.map((blog) => (
+            <Card blog={blog} key={blog._id} />
+          ))
+  
+    }
     </div>
   )
 }
