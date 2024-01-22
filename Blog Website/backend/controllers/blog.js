632e0875525dd64b1aebffa9d4338fca2b606491
Abhishek <abhisheksunny463@gmail.com>
2024-01-22 22:06:47 +0530

    Blog Website

--- Blog Website/backend/controllers/blog.js
+++ Blog Website/backend/controllers/blog.js
@@ -15,7 +15,7 @@ const handleDeleteBlog = async()=> {
 
 const handleGetAllBlog = async (req, res) => {
     const blogs = await Blog.find({})
-    res.json(blogs)
+    res.status(200).json(blogs)
 }
 
 module.exports = {handleAddBlog, handleDeleteBlog, handleGetAllBlog}
