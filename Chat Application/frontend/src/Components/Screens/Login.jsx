c2973ed5e364e8f584558c82c269ccf79492d162
Abhishek <abhisheksunny463@gmail.com>
2024-01-28 03:02:56 +0530

    Write all the functionality for singup and login in backend using jwt token and bcrypt

--- Chat Application/frontend/src/Components/Screens/Login.jsx
+++ Chat Application/frontend/src/Components/Screens/Login.jsx
@@ -1,22 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import {
-    FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, VStack, Button,
+    Spinner, FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, VStack, Button,
 } from '@chakra-ui/react'
 
 const Login = () => {
-  return (
-    <VStack spacing='5px'>
+
+    const [formData, seFormData] = useState({
+        email: '',
+        password: ''
+    })
+    const [loading, setLoading] = useState(false)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        seFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            setLoading(true)
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+            const response = await axios.post('http://localhost:6001/api/auth/login', formData, config)
+            localStorage.setItem("token", JSON.stringify(response.data.token))
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <VStack spacing='5px'>
             <FormControl isRequired>
                 <FormLabel mt='15px' mb='0px'>Email address</FormLabel>
-                <Input placeholder='example@gmail.com' variant='flushed' type='email' />
+                <Input placeholder='example@gmail.com' variant='flushed' type='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                />
             </FormControl>
             <FormControl isRequired>
                 <FormLabel mt='15px' mb='0px'>Password</FormLabel>
-                <Input placeholder='Password' variant='flushed' type='password' />
+                <Input placeholder='Password' variant='flushed' type='password'
+                    name='password'
+                    value={formData.password}
+                    onChange={handleChange}
+                />
             </FormControl>
-            <Button colorScheme='telegram' w='100%' mt='20px' type='submit' >Sign Up</Button>
+            <Button colorScheme='telegram' w='100%' mt='20px' type='submit' onClick={handleSubmit}>{loading ? (<Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='lg'
+            />) : 'Sign Up'}</Button>
         </VStack>
-  )
+    )
 }
 
 export default Login
--- Chat Application/frontend/src/Components/Screens/Signup.jsx
+++ Chat Application/frontend/src/Components/Screens/Signup.jsx
@@ -2,6 +2,7 @@ import {
     FormControl, FormLabel, FormErrorMessage, FormHelperText, Input, VStack, Button,
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import axios from 'axios'
 
 // CLOUDINARY_URL=cloudinary://254422416915595:YbMjjS3h8SyxEKTiXQVBPn36MRE@deprtb7in
 
@@ -11,60 +12,83 @@ const Signup = () => {
         name: '',
         email: '',
         password: '',
-        profileImage: ''
+        profileImage: null
     })
     const [loading, setLoading] = useState(false)
 
     const handleChange = (e) => {
-        const {name, value, files} = e.target
+        const { name, value, files } = e.target
         const file = files && files[0]
-        seFormData((prevData)=> ({
+        seFormData((prevData) => ({
             ...prevData,
             [name]: name === 'profileImage' ? file : value,
         }))
     }
 
-    const handleSubmit = async () => {
-        setLoading(true)
-        console.log("data", formData)
-        setLoading(false)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            setLoading(true)
+            const formDataToSend = new FormData();
+            formDataToSend.append('name', formData.name);
+            formDataToSend.append('email', formData.email);
+            formDataToSend.append('password', formData.password);
+            formDataToSend.append('profileImage', formData.profileImage);
+
+            const config = {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+
+            const response = await axios.post('http://localhost:6001/api/auth/signup', formDataToSend, config)
+
+            console.log("response", response)
+            setLoading(false)
+
+
+        } catch (error) {
+            setLoading(false)
+            console.log("error", error)
+        }
+
     }
 
     return (
         <VStack spacing='5px'>
-            <FormControl isRequired>
-                <FormLabel mb='0px'>User name</FormLabel>
-                <Input placeholder='John Doe' type='text' variant='flushed'
-                name='name'
-                value={formData.name}
-                onChange={handleChange}
-                />
-            </FormControl>
-            <FormControl isRequired>
-                <FormLabel mt='15px' mb='0px'>Email address</FormLabel>
-                <Input placeholder='example@gmail.com' variant='flushed' type='email'
-                name='email'
-                value={formData.email}
-                onChange={handleChange}
-                />
-            </FormControl>
-            <FormControl isRequired>
-                <FormLabel mt='15px' mb='0px'>Password</FormLabel>
-                <Input placeholder='Password' variant='flushed' type='password'
-                name='password'
-                value={formData.password}
-                onChange={handleChange}
-                />
-            </FormControl>
-            <FormControl>
-                <FormLabel mt='15px'>Profile Image</FormLabel>
-                <Input placeholder=' Confirm Password' variant='flushed' type='file' accept='image/*' 
-                name='profileImage'
-                value={formData.profileImage}
-                onChange={handleChange}
-                />
-            </FormControl>
-            <Button colorScheme='telegram' w='100%' mt='20px' type='submit' onClick={handleSubmit} >Sign Up</Button>
+                <FormControl isRequired>
+                    <FormLabel mb='0px'>User name</FormLabel>
+                    <Input placeholder='John Doe' type='text' variant='flushed'
+                        name='name'
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
+                </FormControl>
+                <FormControl isRequired>
+                    <FormLabel mt='15px' mb='0px'>Email address</FormLabel>
+                    <Input placeholder='example@gmail.com' variant='flushed' type='email'
+                        name='email'
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </FormControl>
+                <FormControl isRequired>
+                    <FormLabel mt='15px' mb='0px'>Password</FormLabel>
+                    <Input placeholder='Password' variant='flushed' type='password'
+                        name='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </FormControl>
+                <FormControl>
+                    <FormLabel mt='15px'>Profile Image</FormLabel>
+                    <Input type='file' accept='image/*' variant='flushed'
+                        name='profileImage'
+                        onChange={handleChange}
+                    />
+                </FormControl>
+                <Button colorScheme='telegram' w='100%' mt='20px' type='submit' onClick={handleSubmit}>Sign Up</Button>
         </VStack>
     )
 }
