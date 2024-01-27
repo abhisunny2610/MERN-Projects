3c9cc0a8689cfa45cdbbf8b6013c2769fa531725
Abhishek <abhisheksunny463@gmail.com>
2024-01-27 13:47:32 +0530

    Blog Website

--- Blog Website/frontend/src/Components/Header.jsx
+++ Blog Website/frontend/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../Assests/logo-no-background.png'
 import { AuthContext } from '../Context/AuthContext'
@@ -26,18 +26,18 @@ const Header = () => {
 
   return (
     <>
-      <header className='d-flex align-items-center justify-content-between' >
+      <header className={`d-flex align-items-center justify-content-between ${scrollPosition > 150 ? 'white-background' : ''} `} >
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <div className="links d-flex gap-4">
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
-          <Link to='/writeBlog'>Write Blog</Link>
         </div>
         <div className="account d-flex gap-4">
+          <Link to='/writeBlog'>Write Blog</Link>
           <Link to='/ourBlogs'>Our Blogs</Link>
-          <Link>Sign in</Link>
+          <Link to='/signin'>Sign in</Link>
         </div>
       </header>
     </>
