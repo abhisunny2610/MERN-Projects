28605983b036a09f0c91f99ffb1efa038cefd84b
Abhishek <abhisheksunny463@gmail.com>
2024-02-05 18:38:32 +0530

    Update group name and users in group

--- Chat Application/frontend/src/Components/Small Components/UpdateGroupModel.jsx
+++ Chat Application/frontend/src/Components/Small Components/UpdateGroupModel.jsx
@@ -15,11 +15,13 @@ import {
     Badge,
     FormControl,
     Input,
-    Spinner
+    Spinner,
+    Flex
 } from '@chakra-ui/react'
 import { ChatState } from '../../Context/ChatProvider'
 import axios from 'axios'
 import { base_url } from '../../Utils/Helper'
+import UserList from './UserList'
 
 const UpdateGroupModel = ({ fetchAgain, setFetchAgain }) => {
 
@@ -33,14 +35,50 @@ const UpdateGroupModel = ({ fetchAgain, setFetchAgain }) => {
     const [searchInput, setSearchInput] = useState() // for search input
     const [searchResult, setSearchResult] = useState([]) // list of user using search
     const [loading, setLoading] = useState(false) // loading
+    const [renameLoading, setRenameLoading] = useState(false) // loading
 
-    const handleRemoveUser = (user) => {
-
-    }
 
     const handleRename = async () => {
 
-        if(!groupName) return
+        if (!groupName) return
+
+        try {
+            setRenameLoading(true)
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            }
+
+            const { data } = await axios.put(base_url + "chat/renamegroup", {
+                chatId: selectedChat._id,
+                chatName: groupName
+            }, config)
+
+            setSelectedChat(data)
+            setFetchAgain(!fetchAgain)
+            setRenameLoading(false)
+        } catch (error) {
+            toast({
+                title: "Failed to Create the Chat!",
+                description: error.response.data,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setRenameLoading(false)
+        }
+        setGroupName("")
+        onClose()
+    }
+
+    const handleSearch = async (query) => {
+        setSearchInput(query)
+
+        if (!query) {
+            return;
+        }
 
         try {
             setLoading(true)
@@ -50,14 +88,11 @@ const UpdateGroupModel = ({ fetchAgain, setFetchAgain }) => {
                 }
             }
 
-            const {data} = await axios.put(base_url + "chat/renamegroup", {
-                chatId: selectedChat._id,
-                chatName: groupName
-            }, config)
-
-            setSelectedChat(data)
-            setFetchAgain(!fetchAgain)
+            // getting all the users 
+            const response = await axios.get(base_url + `auth/allUsers?search=${searchInput}`, config)
             setLoading(false)
+            setSearchResult(response?.data?.users)
+
         } catch (error) {
             toast({
                 title: "Failed to Create the Chat!",
@@ -66,16 +101,111 @@ const UpdateGroupModel = ({ fetchAgain, setFetchAgain }) => {
                 duration: 5000,
                 isClosable: true,
                 position: "bottom",
-              });
-              setLoading(false)
+            });
+            setLoading(false)
         }
-        setGroupName("")
-        onClose()
-    }
-
-    const handleSearch = () => {
 
     }
+
+    // to make a users array to make a guorp members
+    const handleAddUser = async (user1) => {
+        if (selectedUser.users.find((u) => u._id === user1._id)) {
+            toast({
+                title: "User already exists",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            })
+            return;
+        }
+
+        if (selectedUser.groupAdmin._id !== user._id) {
+            toast({
+                title: "Only admin can add someone!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom"
+            })
+            return;
+        }
+
+        try {
+            setLoading(true)
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            }
+
+            const { data } = await axios.put(base_url + "chat/addingroup", {
+                chatId: selectedChat._id,
+                userId: user1._id
+            }, config)
+
+            setSelectedChat(data)
+            setFetchAgain(!fetchAgain)
+            setRenameLoading(false)
+
+        } catch (error) {
+            toast({
+                title: "Failed to Create the Chat!",
+                description: error.response.data,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            })
+            setLoading(false)
+        }
+    }
+
+    const handleRemoveUser = async (user1) => {
+        if (selectedChat.groupAdmin._id !== user._id && user1._id !== user._id) {
+            toast({
+                title: "Only admins can remove someone!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+
+        try {
+            setLoading(true);
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+            const { data } = await axios.put(
+                base_url + "chat/removefromgroup",
+                {
+                    chatId: selectedChat._id,
+                    userId: user1._id,
+                },
+                config
+            );
+
+            user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
+            setFetchAgain(!fetchAgain);
+            setLoading(false);
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+        }
+        setGroupName("");
+    }
+
 
     return (
         <>
@@ -99,13 +229,22 @@ const UpdateGroupModel = ({ fetchAgain, setFetchAgain }) => {
                             <Input type='text' placeholder='Rename Group' value={groupName} onChange={(e) => setGroupName(e.target.value)} size="sm" />
                             <Button ml="2" colorScheme='green' size="sm" onClick={handleRename}>
                                 {
-                                    loading? <Spinner size='xs' color='white' /> : "Rename"
+                                    renameLoading ? <Spinner size='xs' color='white' /> : "Rename"
                                 }
                             </Button>
                         </FormControl>
                         <FormControl mt={2} mb={2}>
                             <Input type='text' size='sm' placeholder='Search User' onChange={(e) => handleSearch(e.target.value)}></Input>
                         </FormControl>
+                        {
+                            loading ? <Flex justifyContent="center" alignItems="center">
+                                <Spinner />
+                            </Flex> : (
+                                searchResult?.slice(0, 4).map((user) => {
+                                    return <UserList user={user} key={user._id} handleFunction={() => handleAddUser(user)} />
+                                })
+                            )
+                        }
                     </ModalBody>
 
                     <ModalFooter>
