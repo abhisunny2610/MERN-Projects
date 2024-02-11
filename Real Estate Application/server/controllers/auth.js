85e5a5d3ca881c83d542b8c2d48819cc655e3fbe
Abhishek <abhisheksunny463@gmail.com>
2024-02-11 23:18:25 +0530

    Creating auth api endpoints

--- Real Estate Application/server/controllers/auth.js
+++ Real Estate Application/server/controllers/auth.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/user")
 const { generateToken } = require("../utils/auth/token")
+const bcrypt = require("bcrypt")
 
 // for register
 const register = asyncHandler(async (req, res, next) => {
@@ -8,7 +9,7 @@ const register = asyncHandler(async (req, res, next) => {
 
     // check all the fileds 
     if (!firstName || !lastName || !email || !password || !phoneNumber) {
-        return res.status(400).json({ error: "Please filled all the fields." })
+        return res.status(400).json({ error: "Please fill all the fields." })
     }
 
     try {
@@ -30,4 +31,29 @@ const register = asyncHandler(async (req, res, next) => {
     }
 })
 
-module.exports = { register }
+const login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email && !password) {
+        return res.status(400).json({ msg: "Please fill all the fields." })
+    }
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ msg: "User not found" })
+        }
+
+        const validatePassword = bcrypt.compare(password, user.password)
+
+        if (validatePassword) {
+            const token = await generateToken(user)
+            return res.status(200).json({ msg: "User successfully login", token: token })
+        }
+
+    } catch (error) {
+        return res.status(500).send("Server Error")
+    }
+})
+
+module.exports = { register, login }
